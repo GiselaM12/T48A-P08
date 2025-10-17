7d357def570062d2f8f5cfa4b3bf7e557e7c8f50
GiselaM12 <100981841+GiselaM12@users.noreply.github.com>
2025-10-17 16:22:19 -0600

    Update test_model.py

--- naive-bayes-classifier/tests/test_model.py
+++ naive-bayes-classifier/tests/test_model.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.model import model, y_pred, y_test
+from src.model import model, y_pred, y_test, df
 
 
 
